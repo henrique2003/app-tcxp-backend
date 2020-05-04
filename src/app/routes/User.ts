09b9f5da8e48834c14cb6cdf2908837a5ef3b14c
Henrique de melo <henrique.de.melo.cristioglu@gmail.com>
2020-05-04 19:56:03 -0300

    feat: add User routes

--- src/app/routes/User.ts
+++ src/app/routes/User.ts
@@ -1,5 +1,19 @@
 import { Router } from 'express'
+import UserController from '../controllers/user-controller'
+import { auth, emailConfirmation, userExists } from '../middlewares'
 
 const routes = Router()
+
+// List all Users
+// Store
+routes.post('/users', UserController.store)
+// Show
+routes.get('/user/:id', UserController.show)
+// Index
+routes.get('/users', auth, UserController.index)
+// Update
+routes.put('/users', auth, userExists, emailConfirmation, UserController.update)
+// Destroy
+routes.delete('/users', auth, userExists, emailConfirmation, UserController.destroy)
 
 export default routes
