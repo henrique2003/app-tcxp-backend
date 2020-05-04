2b097b1f267bdf92a7b0b787515cd677c96c0893
Henrique de melo <henrique.de.melo.cristioglu@gmail.com>
2020-05-04 19:27:18 -0300

    feat: add middleware emailConfirmation

--- src/app/middlewares/email-confirmation.ts
+++ src/app/middlewares/email-confirmation.ts
@@ -1,16 +1,16 @@
-// import { Request, Response, NextFunction } from 'express'
-// import { User } from '../models'
+import { Request, Response, NextFunction } from 'express'
+import { User } from '../models'
 
-// const emailConfirmation = async (req: Request, res: Response, next: NextFunction) => {
-//   try {
-//     if (!await User.findOne({ _id: req.userId, emailConfirmation: true })) {
-//       return res.status(401).json('Necessário comfirmar email')
-//     }
+const emailConfirmation = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    if (!await User.findOne({ _id: req.userId, emailConfirmation: true })) {
+      return res.status(401).json('Necessário comfirmar email')
+    }
 
-//     next()
-//   } catch (error) {
-//     return res.status(500).json('Server Error')
-//   }
-// }
+    next()
+  } catch (error) {
+    return res.status(500).json('Server Error')
+  }
+}
 
-// export default emailConfirmation
+export default emailConfirmation
