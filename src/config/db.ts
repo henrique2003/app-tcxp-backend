ed8dd6c9e1d648f9e17f0bf5ca0012b63c99991c
Henrique de melo <henrique.de.melo.cristioglu@gmail.com>
2020-05-04 17:49:28 -0300

    feat: add Db config

---
+++ src/config/db.ts
@@ -0,0 +1,21 @@
+import { connect } from 'mongoose'
+require('dotenv').config()
+
+class Db {
+  public async connectDb (): Promise<void> {
+    try {
+      await connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true
+      })
+
+      console.log('MongoDb Connected...')
+    } catch (err) {
+      console.error(err.message)
+      process.exit(1)
+    }
+  }
+}
+
+export default new Db()
