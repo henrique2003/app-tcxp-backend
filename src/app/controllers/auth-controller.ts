780dcadd09c39e2d5bf26f082a89c8ca087736c5
Henrique de melo <henrique.de.melo.cristioglu@gmail.com>
2020-05-04 20:02:34 -0300

    feat: add AuthController

---
+++ src/app/controllers/auth-controller.ts
@@ -0,0 +1,43 @@
+import { Request, Response } from 'express'
+import { compare } from 'bcrypt'
+import { User } from '../models'
+import { generateToken, isValidFields, cleanFields } from '../../utils'
+
+class AuthController {
+  public async login (req: Request, res: Response): Promise<Response> {
+    try {
+      const { email, password } = req.body
+
+      req.body = cleanFields(req.body)
+
+      const requiredFields = ['email', 'password']
+      if (!isValidFields(requiredFields, req.body)) {
+        return res.status(400).json('Campo em branco')
+      }
+
+      const user = await User.findOne({ email }).select('+password')
+
+      if (!user || !await compare(password, user.password)) {
+        return res.status(400).json('Usário ou senha incorreto')
+      }
+
+      Object.assign({}, user, { password: undefined })
+
+      return res.status(200).json({ user, token: generateToken(user.id) })
+    } catch (error) {
+      return res.status(500).json('Server Error')
+    }
+  }
+
+  public async loadUser (req: Request, res: Response): Promise<Response> {
+    try {
+      const user = await User.findById(req.userId)
+
+      return res.status(200).json(user)
+    } catch (error) {
+      return res.status(500).json('Server Error')
+    }
+  }
+}
+
+export default new AuthController()
