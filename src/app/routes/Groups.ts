881a31ba506339a4ed089857c026582689b739cb
Henrique de melo <henrique.de.melo.cristioglu@gmail.com>
2020-05-04 20:35:59 -0300

    feat: add Group routes

--- src/app/routes/Groups.ts
+++ src/app/routes/Groups.ts
@@ -1,5 +1,16 @@
 import { Router } from 'express'
+import { GroupController } from '../controllers'
+import { auth, emailConfirmation, userExists } from '../middlewares'
 
 const router = Router()
+
+// Index all groups
+router.get('/groups', GroupController.index)
+// Create a new Group
+router.post('/groups', auth, emailConfirmation, GroupController.store)
+// Index your groups
+router.get('/groups/mine', auth, emailConfirmation, userExists, GroupController.mine)
+// Show especify group
+router.get('/group/:id', GroupController.show)
 
 export default router
