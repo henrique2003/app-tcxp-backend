e282fe62be6b99d5a406884f1eeb238a1d566b5e
Henrique de melo <henrique.de.melo.cristioglu@gmail.com>
2020-05-05 16:39:16 -0300

    fix: resolve type fix mongo url

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,15 +1,17 @@
 import { connect } from 'mongoose'
-require('dotenv').config()
+import { config } from 'dotenv'
+config()
 
 class Db {
+  private readonly mongo_url = process.env.MONGO_URL ?? 'mongodb://localhost:27017/app-viagem'
+
   public async connectDb (): Promise<void> {
     try {
-      await connect('mongodb://localhost:27017/app-viagem', {
+      await connect(this.mongo_url, {
         useNewUrlParser: true,
         useCreateIndex: true,
         useUnifiedTopology: true
       })
-
       console.log('MongoDb Connected...')
     } catch (err) {
       console.error(err.message)
