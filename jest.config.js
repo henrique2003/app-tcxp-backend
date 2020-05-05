bdadf2b28a941fd6e0f1f5d87876a05a64e8a7c1
Henrique de melo <henrique.de.melo.cristioglu@gmail.com>
2020-05-05 09:11:35 -0300

    chore: add unit and integration config jest

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,9 @@
-// For a detailed explanation regarding each configuration property, visit:
-// https://jestjs.io/docs/en/configuration.html
-
 module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
-  testMatch: ['**/*.test.ts'],
   transform: {
     '.+\\.ts$': 'ts-jest'
   }
-};
+}
