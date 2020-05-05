d62573f92a8fd85c62cfd8bf09f53b092378109b
Henrique de melo <henrique.de.melo.cristioglu@gmail.com>
2020-05-05 20:48:58 -0300

    refactor: change group routes name

--- src/app/routes/group.ts
+++ src/app/routes/group.ts
@@ -2,15 +2,15 @@ import { Router } from 'express'
 import { GroupController } from '../controllers'
 import { auth, emailConfirmation } from '../middlewares'
 
-const router = Router()
+const routes = Router()
 
 // Index all groups
-router.get('/groups', GroupController.index)
+routes.get('/groups', GroupController.index)
 // Create a new Group
-router.post('/groups', auth, emailConfirmation, GroupController.store)
+routes.post('/groups', auth, emailConfirmation, GroupController.store)
 // Index your groups
-router.get('/groups/mine', auth, emailConfirmation, GroupController.mine)
+routes.get('/groups/mine', auth, emailConfirmation, GroupController.mine)
 // Show especify group
-router.get('/group/:id', GroupController.show)
+routes.get('/group/:id', GroupController.show)
 
-export default router
+export default routes
