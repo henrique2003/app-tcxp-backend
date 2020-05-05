32d400c0131db06f2066f8726c5b04f32205edba
Henrique de melo <henrique.de.melo.cristioglu@gmail.com>
2020-05-05 20:54:35 -0300

    feat: add route loadUser

--- src/app/routes/Auth.ts
+++ src/app/routes/Auth.ts
@@ -1,8 +1,12 @@
 import { Router } from 'express'
 import { AuthController } from '../controllers'
+import { auth } from '../middlewares'
 
 const routes = Router()
 
+// Login
 routes.post('/login', AuthController.login)
+// Load user
+routes.get('/load', auth, AuthController.loadUser)
 
 export default routes
