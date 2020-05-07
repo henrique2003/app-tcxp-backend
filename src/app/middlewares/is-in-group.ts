3db0af2556327178bec30489c33e5639000480b5
Henrique de melo <henrique.de.melo.cristioglu@gmail.com>
2020-05-07 18:27:31 -0300

    feat: ensure isInGroup return 404 if field group is invalid

--- src/app/middlewares/is-in-group.ts
+++ src/app/middlewares/is-in-group.ts
@@ -1,11 +1,15 @@
 import { Request, Response, NextFunction } from 'express'
 import { Groups } from '../models'
-import { serverError } from '../errors'
+import { serverError, notFound } from '../errors'
 
 export const isInGroup = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const { body, userId } = req
     const { group } = body
+
+    if (group.length !== 24) {
+      return res.status(404).json(notFound('Convite'))
+    }
 
     if (!await Groups.findOne({ _id: group, creator: userId })) {
       if (!await Groups.findOne({ _id: group, administrator: userId })) {
