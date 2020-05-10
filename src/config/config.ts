5e515b45bffad64ab58ba1b473f1522a11e7cfde
Henrique de melo <henrique.de.melo.cristioglu@gmail.com>
2020-05-10 14:31:24 -0300

    refactor: move config secret token to a file config

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,7 +3,7 @@ import { config } from 'dotenv'
 config()
 
 export default {
-  secret: '9d123a0ebf7a356232269e46230159cc',
+  secret: process.env.SECRET_TOKEN ?? '',
   s3: new S3({
     accessKeyId: process.env.AWS_KEI_ID,
     secretAccessKey: process.env.AWS_SECRET_KEY,
