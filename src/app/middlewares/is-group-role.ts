cdabfdddeaaddbab657311e3df3c4b1604e3d034
Henrique de melo <henrique.de.melo.cristioglu@gmail.com>
2020-05-16 19:10:44 -0300

    feat: add isCreatorOrAdmin method

--- src/app/middlewares/is-group-role.ts
+++ src/app/middlewares/is-group-role.ts
@@ -29,7 +29,7 @@ export const isAdmin = async (req: Request, res: Response, next: NextFunction) =
     }
 
     if (!await Groups.findOne({ _id: params.id, administrators: userId })) {
-      return res.status(401).json('Acesso negado')
+      return res.status(401).json('Acesso negadoc')
     }
 
     return next()
@@ -48,6 +48,27 @@ export const isCreator = async (req: Request, res: Response, next: NextFunction)
 
     if (!await Groups.findOne({ _id: params.id, creator: userId })) {
       return res.status(401).json('Acesso negado')
+    }
+
+    return next()
+  } catch (error) {
+    return res.status(500).json(serverError())
+  }
+}
+
+export const isCreatorOrAdmin = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { params, userId } = req
+    const { id } = params
+
+    if (!params.id) {
+      return res.status(400).json(notFound('Id'))
+    }
+
+    if (!await Groups.findOne({ _id: id, creator: userId })) {
+      if (!await Groups.findOne({ _id: id, administrators: userId })) {
+        return res.status(401).json('Acesso negadoc')
+      }
     }
 
     return next()
